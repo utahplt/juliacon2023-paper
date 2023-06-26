64e8510f83f9607d2070bc361abcb43763b19e46
Ashton Wiersdorf <mail@wiersdorf.dev>
2023-06-26 11:59:47 -0600

    Little Racket program to make graphs for paper/talk

---
+++ more_figures.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(require plot)
+
+;; (define (floor-log n [base 10])
+;;   (floor (log n base)))
+
+(define (fp-example-points num subseq [stretch 2])
+  (define last-val 0)
+  (for/list ([i (range 0 num)])
+    (set! last-val (+ last-val (expt stretch (floor (/ i subseq)))))
+    last-val))
+
+(parameterize ([plot-width 700]
+               [plot-height 20])
+  (define xs (append (map - (fp-example-points 32 8 1.8))
+                     (list 0)
+                     (fp-example-points 32 8 1.8)))
+  (plot (points (map (λ (x) (vector x 0)) xs)
+                #:sym 'point)))
